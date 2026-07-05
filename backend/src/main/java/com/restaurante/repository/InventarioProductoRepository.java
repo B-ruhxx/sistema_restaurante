b0a45ce267f8a3cff0661d27f2e7992752d3d9f6
B-ruhxx <davinote04@gmail.com>
2026-07-04 19:16:15 -0500

    refactor(backend): eliminar lógica y entidades de VarianteProducto e InventarioProducto

--- backend/src/main/java/com/restaurante/repository/InventarioProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.restaurante.repository;
-
-import com.restaurante.entity.InventarioProducto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface InventarioProductoRepository extends JpaRepository<InventarioProducto, Integer> {
-    Optional<InventarioProducto> findByProductoIdProducto(Integer idProducto);
-}
--- backend/src/main/java/com/restaurante/repository/VarianteProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.restaurante.repository;
-
-import com.restaurante.entity.VarianteProducto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface VarianteProductoRepository extends JpaRepository<VarianteProducto, Integer> {
-    List<VarianteProducto> findByProductoIdProducto(Integer idProducto);
-}
